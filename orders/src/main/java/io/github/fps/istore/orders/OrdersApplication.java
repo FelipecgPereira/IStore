7dfda479834d99f14a91a5debc6156dcd99ec349
FelipecgPereira <felipepereiracg@gmail.com>
2025-11-22 17:03:48 -0300

    config kafka

--- orders/src/main/java/io/github/fps/istore/orders/OrdersApplication.java
+++ orders/src/main/java/io/github/fps/istore/orders/OrdersApplication.java
@@ -1,10 +1,18 @@
 package io.github.fps.istore.orders;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class OrdersApplication {
+
+//    @Bean
+//    public CommandLineRunner commandLineRunner(KafkaTemplate<String,String>template){
+//      return args ->   template.send("istore.orders-paid", "data","{json}");
+//    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrdersApplication.class, args);
